2cf3ef2df8029cefe86ef084571da0aa38136751
Leonardo Gonzalez <gonzalezrujano@gmail.com>
2018-11-01 22:59:10 -0400

    feat: Clase de extraccion de Informacion del Documento

--- src/com/corex/sisconot/Escaneo.kt
+++ src/com/corex/sisconot/Escaneo.kt
@@ -3,6 +3,7 @@ package com.corex.sisconot
 import java.io.File
 import java.io.IOException
 import java.util.*
+import kotlin.collections.ArrayList
 
 class Imagen(nombre: String, path: String) { val Ubicacion: String = "$path/$nombre" }
 
@@ -16,28 +17,56 @@ class Escaneo(nombreImagen: String, pathImagen: String) {
             val proceso: Process = Runtime.getRuntime().exec(Comando)
             println("Extrayendo Texto")
             proceso.waitFor()
-        } catch (e: IOException) { println("Error en la ejecucion del Tesseract") }
+        } catch (e: IOException) { println("Error: Durante la ejecucion del Tesseract") }
     }
 }
 
-class Extraccion(UbicacionFichero: String) {
-    val Ubicacion: String = UbicacionFichero
+class Extraccion(Ubicacion: String) {
     var Archivo: File = File("$Ubicacion/outputbase.txt")
     var ContenidoArchivo: Scanner = Scanner(Archivo)
-
-    init { LeerContenido() }
+    var Busqueda: String = ""
+    var LineaActual: String = ""
+    var NumeroDeResultados : Int = 0
+    var NumeroDeIteraciones : Int = 0
+    var ListaResultados : ArrayList<String> = ArrayList()
 
     fun LeerContenido() {
         while (ContenidoArchivo.hasNextLine()) {
-            println(ContenidoArchivo.nextLine())
+            LineaActual = ContenidoArchivo.nextLine()
+            RevisarLinea()
+            if (NumeroDeResultados == NumeroDeIteraciones) {
+                break
+            }
         }
         ContenidoArchivo.close()
     }
+
+    fun RevisarLinea() {
+        if (LineaActual.contains(Busqueda)) {
+            AumentarResultados()
+            GuardarLinea()
+        }
+    }
+
+    fun AumentarResultados() {
+        NumeroDeResultados ++
+    }
+
+    fun GuardarLinea() {
+        ListaResultados.add(LineaActual)
+    }
+
 }
 
 fun main(args: Array<String>) {
     println("Comando de Tesseract")
 
-    //Escaneo("Scan.JPG", "/home/leonardo/Descargas")
-    Extraccion("/home/leonardo/Descargas")
+    val Ubicacion : String = "/home/leonardo/Descargas"
+    //Escaneo("Scan.JPG", Ubicacion)
+    var extraccion = Extraccion(Ubicacion)
+    extraccion.Busqueda = "CASTELLANO"
+    extraccion.NumeroDeIteraciones = 3
+    extraccion.LeerContenido()
+    println(extraccion.ListaResultados.get(1))
+
 }
